f2a5522013a3806602e12bb16664489470864b4e
munrhalls <antarcticdepths@gmail.com>
2021-03-02 17:02:08 +0100

    refactor to domino flow

--- to-do.js
+++ to-do.js
@@ -1,43 +1,83 @@
-function addRow() {
-  function getInputText() {
-    let input = document.getElementsByTagName('input')[0];
-    return input.value;
-  }
-  function appendText(item, text) {
-    let textNode = document.createTextNode(text);
-    item.appendChild(textNode);
-  }
-  function getList() {
-    let list = document.getElementsByTagName('ul')[0];
-    return list;
-  }
-  function appendToList(list, item) {
-    list.appendChild(item);
-  }
-  function aggregate() {
-    let text = getInputText();
-    let item = createItem();
-    appendText(item, text);
+function append(element) {
+  var main = document.getElementsByTagName('main')[0];
+  main.appendChild(element);
+}
+function createListInterface() {
+  var interface = document.createElement('div');
+  interface.style.display = 'flex';
 
-    let list = getList();
-    appendToList(list, item);
+  function createTextField() {
+    var textField = document.createElement('input');
+    textField.style.type = 'text';
+    textField.style.width = '45%';
+    textField.style.height = '1.5rem';
+    textField.style.border = '3px solid #000';
+    textField.style.borderRadius = '0.5rem';
+    textField.style.textDecoration = 'none';
+    textField.style.padding = '0.25rem 0.5rem';
+    textField.style.fontSize = '1rem';
+    interface.appendChild(textField);
   }
-  aggregate();
+  // in congruence with domino flow
+  function createAddBtn() {
+    var addBtn = document.createElement('div');
+    let sign = document.createElement('div');
+    sign.innerText = '+';
+    sign.style.fontSize = '1rem';
+    sign.style.fontWeight = 'bold';
+    sign.style.margin = '0.25rem';
+    sign.style.color = 'green';
+    addBtn.appendChild(sign);
+    addBtn.style.textAlign = 'center';
+    addBtn.style.width = '1.5rem';
+    addBtn.style.height = '1.5rem';
+    addBtn.style.border = '6px solid #020202';
+    addBtn.style.borderRadius = '0.5rem';
+    addBtn.style.textDecoration = 'none';
+    addBtn.style.backgroundColor = '#000000';
+    interface.appendChild(addBtn);
+  }
+  createTextField();
+  createAddBtn();
+  append(interface);
+}
+
+function createList() {
+  var list = document.createElement('ul');
+  append(list);
 }
 function createItem() {
-  let li = document.createElement('li');
-  return li;
+  var item = document.createElement('li');
+  var input = document.getElementsByTagName('input')[0];
+  var textNode = document.createTextNode(input.value);
+  var span = document.createElement('span');
+  var delBtn = createDelBtn();
+
+  span.appendChild(textNode);
+  item.appendChild(span);
+  item.appendChild(delBtn)
 }
+
 function createDelBtn() {
-  let delBtn = document.createElement('button');
+  var delBtn = document.createElement('span');
+  delBtn.innerText = 'x';
+
+  delBtn.onclick = function(e) {
+    console.log('x')
+  }
+  return delBtn;
 }
-function appendDelBtn(row) {
-  let delBtn = createDelBtn();
-  row.appendChild(delBtn);
-}
-}
+
 window.addEventListener('keydown', function(e) { 
   if (e.code == 'Enter') {
-    addRow();
+    createItem();
   }
 });
+
+function aggregate() {
+  createListInterface();
+  createList();
+}
+window.onload = function() {
+  aggregate();
+};
